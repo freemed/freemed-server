6d2cb47d0b7189436bb5c54095ec0cdb23a89ccd
Jeffrey Buchbinder <jbuchbinder@ravemobilesafety.com>
2020-06-16 17:26:50 -0400

    API/Billing/DB: Build out config and REMITT interfacing

---
+++ billing/remitt.go
@@ -0,0 +1,50 @@
+package billing
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/freemed/freemed-server/model"
+	rc "github.com/freemed/remitt-server/client"
+	rm "github.com/freemed/remitt-server/model"
+)
+
+// RemittClient is the local FreeMED wrapper around the REMITT base client code
+type RemittClient struct {
+	Client *rc.RemittClient
+}
+
+// init initializes the internal RemittClient instance with URLs, credentials, etc.
+func (r *RemittClient) init() error {
+	url, err := model.ConfigGetByKey("remitt_url")
+	if err != nil {
+		return err
+	}
+	user, err := model.ConfigGetByKey("remitt_user")
+	if err != nil {
+		return err
+	}
+	pass, err := model.ConfigGetByKey("remitt_pass")
+	if err != nil {
+		return err
+	}
+	r.Client, err = rc.NewClient(user.Value.String, pass.Value.String, url.Value.String)
+	return nil
+}
+
+// SubmitPayload submits a payload to a local REMITT instance
+func (r *RemittClient) SubmitPayload(billkey int64, render, renderOption, transport, transportOption string) (int64, error) {
+	payload, err := model.GetBillkeyPayload(billkey)
+	if err != nil {
+		log.Printf("SubmitPayload(%d): ERROR: %s", billkey, err.Error())
+		return 0, err
+	}
+	return r.Client.PayloadInsert(rc.InputPayload{
+		OriginalID:      rm.NewNullStringValue(fmt.Sprintf("%d", billkey)),
+		RenderPlugin:    render,
+		RenderOption:    renderOption,
+		TransportPlugin: transport,
+		TransportOption: transportOption,
+		InputPayload:    payload,
+	})
+}
--- model/config.go
+++ model/config.go
@@ -1,11 +1,17 @@
 package model
 
 import (
-//"database/sql"
+	"fmt"
+	"sync"
 )
 
 const (
 	TABLE_CONFIG = "config"
+)
+
+var (
+	configCache     map[int64]ConfigModel
+	configCacheLock *sync.RWMutex
 )
 
 type ConfigModel struct {
@@ -20,4 +26,71 @@ type ConfigModel struct {
 
 func init() {
 	DbTables = append(DbTables, DbTable{TableName: TABLE_CONFIG, Obj: ConfigModel{}, Key: "Id"})
+}
+
+// cacheConfigValues is an internal caching mechanism for reading all config values
+func cacheConfigValues(force bool) error {
+	configCacheLock.Lock()
+	defer configCacheLock.Unlock()
+	if configCache == nil || force {
+		configCache = map[int64]ConfigModel{}
+	}
+	if len(configCache) < 1 || force {
+		var cm []ConfigModel
+		_, err := DbMap.Select(&cm, "SELECT * FROM "+TABLE_CONFIG)
+		if err != nil {
+			return err
+		}
+		for _, v := range cm {
+			configCache[v.Id] = v
+		}
+	}
+	return nil
+}
+
+// ConfigGetBySectionKey returns a config entry based on the specified key and section
+func ConfigGetBySectionKey(section, key string) (ConfigModel, error) {
+	err := cacheConfigValues(false)
+	if err != nil {
+		return ConfigModel{}, err
+	}
+	configCacheLock.RLock()
+	defer configCacheLock.RUnlock()
+	for _, v := range configCache {
+		if v.Section.String == section && v.Key == key {
+			return v, nil
+		}
+	}
+	return ConfigModel{}, fmt.Errorf("config value with section %s and key %s not found", section, key)
+}
+
+// ConfigGetByKey returns a config entry based on the specified key
+func ConfigGetByKey(key string) (ConfigModel, error) {
+	err := cacheConfigValues(false)
+	if err != nil {
+		return ConfigModel{}, err
+	}
+	configCacheLock.RLock()
+	defer configCacheLock.RUnlock()
+	for _, v := range configCache {
+		if v.Key == key {
+			return v, nil
+		}
+	}
+	return ConfigModel{}, fmt.Errorf("config value with key %s not found", key)
+}
+
+// ConfigGetByID returns a config entry based on the specified PK
+func ConfigGetByID(id int64) (ConfigModel, error) {
+	err := cacheConfigValues(false)
+	if err != nil {
+		return ConfigModel{}, err
+	}
+	configCacheLock.RLock()
+	defer configCacheLock.RUnlock()
+	v, found := configCache[id]
+	if !found {
+		return ConfigModel{}, fmt.Errorf("config value with key %d not found", id)
+	}
+	return v, nil
 }
